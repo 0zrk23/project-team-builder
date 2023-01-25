c051e69a3a97c1b64e6b035983d625d76a2a0acb
Gustavo Carrillo <gus.carrillo1221@gmail.com>
2023-01-25 00:55:44 -0700

    fixed issue with prompt validation

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const generateTeam = require('./src/teamGenerator');
 const generateHTML = require('./src/generateHTML');
-const team = require('./db/testGroup.json')
+// const team = require('./db/testGroup.json')
 
 init();
 
@@ -10,9 +10,9 @@ init();
  */
 async function init(){
     //generates team
-    // team = await generateTeam();
+    team = await generateTeam();
     //generates html
-    markup = generateHTML(team);
+    let markup = generateHTML(team);
     //writes the new html file
     fs.writeFile('generated-index.html', markup, (err) =>
       err ? console.log(err) : console.log('Successfully created index.html!')
